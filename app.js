8b15b79e4184bb86f79b5edb90e6c987e2984ad7
mayuri06b <mayuribarapatre9@gmail.com>
2025-02-27 10:27:48 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const num = 189
+if(num  < 1000){
+    console.log("This is small number");
+}else{
+    console.log("This is large Number");
+}
+console.log(`Hey this is my First Node program !!`);
+console.log('John-smilga');
